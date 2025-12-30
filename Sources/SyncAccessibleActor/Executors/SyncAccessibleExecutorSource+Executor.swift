537116d72e9b0881cfc3047cbb1dfd1e8179705e
Carlos <cacmartinez@msn.com>
2025-12-30 16:46:17 -0600

    Clean up

---
+++ Sources/SyncAccessibleActor/Executors/SyncAccessibleExecutorSource+Executor.swift
@@ -0,0 +1,68 @@
+//
+//  SyncAccessibleExecutorSource+Executor.swift
+//  SyncAccessibleActor_private
+//
+//  Created by Compean on 30/12/25.
+//
+
+import Dispatch
+import StrategyDispatchQueue
+
+extension SyncAccessibleExecutorSource {
+    final class Executor: SerialExecutor, TaskExecutor {
+        private let queue: StrategyDispatchQueue
+        private let queueDispatchAction: @Sendable (_ queue: StrategyDispatchQueue, _ qos: DispatchQoS, _ execute: sending @escaping () -> Void) -> Void
+        
+        init(queue: StrategyDispatchQueue, queueDispatchAction: @escaping @Sendable (_ queue: StrategyDispatchQueue, _ qos: DispatchQoS, _ execute: sending @escaping () -> Void) -> Void) {
+            self.queue = queue
+            self.queueDispatchAction = queueDispatchAction
+        }
+        
+        private func qos(from priority: JobPriority) -> DispatchQoS {
+            var qos: DispatchQoS
+            switch priority.rawValue {
+                case (TaskPriority.userInitiated.rawValue+1)...:
+                    qos = .userInteractive
+                case (TaskPriority.medium.rawValue+1)...TaskPriority.userInitiated.rawValue:
+                    qos = .userInitiated
+                case (TaskPriority.utility.rawValue+1)...TaskPriority.medium.rawValue:
+                    qos = .default
+                case (TaskPriority.background.rawValue+1)...TaskPriority.utility.rawValue:
+                    qos = .utility
+                case ...TaskPriority.background.rawValue:
+                    qos = .background
+                default:
+                    qos = .default
+            }
+            return qos
+        }
+        
+        func enqueue(_ job: consuming ExecutorJob) {
+            let jobPriority = job.priority
+            
+            let unownedJob = UnownedJob(job)
+            if meetsDispatchCondition(condition: .onQueue(queue)) {
+                unownedJob.runSynchronously(isolatedTo: self.asUnownedSerialExecutor(),
+                                            taskExecutor: self.asUnownedTaskExecutor())
+            } else {
+                queueDispatchAction(queue, qos(from: jobPriority)) {
+                    print("on async queue: \(self.queue)")
+                    unownedJob.runSynchronously(isolatedTo: self.asUnownedSerialExecutor(),
+                                                taskExecutor: self.asUnownedTaskExecutor())
+                }
+            }
+        }
+        
+        func asUnownedSerialExecutor() -> UnownedSerialExecutor {
+            UnownedSerialExecutor(complexEquality: self)
+        }
+        
+        func asUnownedTaskExecutor() -> UnownedTaskExecutor {
+            UnownedTaskExecutor(ordinary: self)
+        }
+        
+        func checkIsolated() {
+            dispatchPrecondition(condition: .onQueue(queue))
+        }
+    }
+}
